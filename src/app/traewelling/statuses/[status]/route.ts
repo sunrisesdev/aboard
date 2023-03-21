880e454210f96266947c94b882dd44a233335dea
vainamov <fabian@marine.si>
2023-03-21 18:15:31 +0100

    refactor: move token to sdk function

--- src/app/traewelling/statuses/[status]/route.ts
+++ src/app/traewelling/statuses/[status]/route.ts
@@ -1,16 +1,12 @@
-import { authOptions } from '@/pages/api/auth/[...nextauth]';
 import { TraewellingSdk } from '@/traewelling-sdk';
 import createErrorResponse from '@/utils/api/createErrorResponse';
 import createResponse from '@/utils/api/createResponse';
-import { getServerSession } from 'next-auth';
 
 export async function GET(
   _request: Request,
   context: { params: { status: string } }
 ) {
   try {
-    const session = await getServerSession(authOptions);
-
     if (!(context.params.status ?? '').trim()) {
       return createErrorResponse({
         error: 'Invalid parameters',
@@ -19,12 +15,9 @@ export async function GET(
     }
 
     // CAREFUL: Authorization is optional for this function!
-    const data = await TraewellingSdk.status.single(
-      {
-        id: context.params.status,
-      },
-      !session ? undefined : `Bearer ${session.user.accessToken}`
-    );
+    const data = await TraewellingSdk.status.single({
+      id: context.params.status,
+    });
     return createResponse({
       body: data,
     });
