a763261f4bc7095f589b5bb4dbd4cf1a290715ec
vainamov <fabian@marine.si>
2023-03-22 08:14:40 +0100

    wip: add detailed status page

--- src/components/StatusDetails/StatusDetails.tsx
+++ src/components/StatusDetails/StatusDetails.tsx
@@ -2,15 +2,54 @@
 
 import useAccentColor from '@/hooks/useAccentColor/useAccentColor';
 import { useStatus } from '@/hooks/useStatus/useStatus';
+import { useStops } from '@/hooks/useStops/useStops';
+import { Stop } from '@/traewelling-sdk/types';
 import { parseSchedule } from '@/utils/parseSchedule';
-import { MdOutlineToken } from 'react-icons/md';
+import clsx from 'clsx';
+import { useEffect, useMemo, useState } from 'react';
+import { MdCommit, MdOutlineToken } from 'react-icons/md';
 import { TbRoute } from 'react-icons/tb';
 import LineIndicator from '../LineIndicator/LineIndicator';
 import styles from './StatusDetails.module.scss';
-import { StatusDetailsProps } from './types';
+import { CurrentStopProps, StatusDetailsProps } from './types';
+
+const findCurrentStop = (stops: Stop[], destination: Stop) => {
+  const now = Date.now();
+
+  for (const stop of stops) {
+    if (stop.cancelled) {
+      continue;
+    }
+
+    const departure = new Date(stop.departureReal!).getTime();
+
+    if (
+      stop.id === destination.id &&
+      stop.departureReal === destination.departureReal
+    ) {
+      return;
+    }
+
+    if (now > departure) {
+      continue;
+    }
+
+    return stop;
+  }
+};
+
+const getMinutesUntil = (target: Date) => {
+  return Math.ceil((target.getTime() - Date.now()) / 1000 / 60);
+};
 
 const StatusDetails = ({ id }: StatusDetailsProps) => {
   const { status } = useStatus(id);
+  const { stops } = useStops(
+    status?.train.hafasId ?? '',
+    status?.train.lineName ?? '',
+    status?.train.origin.departurePlanned ?? '',
+    status?.train.origin.id.toString() ?? ''
+  );
 
   useAccentColor(`var(--color-${status?.train.category})`);
 
@@ -21,6 +60,12 @@ const StatusDetails = ({ id }: StatusDetailsProps) => {
   if (status === null) {
     return <div>Status nicht gefunden</div>;
   }
+
+  const destination = stops.at(-1);
+
+  const currentStop = findCurrentStop(stops, status.train.destination);
+  const showCurrentStop =
+    !!currentStop && currentStop.name !== status.train.destination.name;
 
   const arrivalSchedule = parseSchedule({
     actual: status.train.destination.arrival,
@@ -33,54 +78,115 @@ const StatusDetails = ({ id }: StatusDetailsProps) => {
   });
 
   return (
-    <section className={styles.base}>
-      <div className={styles.origin}>
-        <div className={styles.station}>
-          <span>{status.train.origin.name}</span>
-          <span className={styles.time}>
-            ab {departureSchedule.planned}
-            {!departureSchedule.isOnTime && (
-              <span className={styles.delay}>
-                &nbsp;<sup>+{departureSchedule.delayInMinutes}</sup>
-              </span>
-            )}
-          </span>
+    <main className={styles.base}>
+      <header className={styles.header}>
+        <div className={styles.direction}>
+          <LineIndicator
+            className={styles.lineIndicator}
+            lineName={status.train.lineName}
+            product={status.train.category}
+            productName=""
+          />
+          <span>{destination?.name}</span>
         </div>
+
+        <div
+          className={clsx(styles.origin, !showCurrentStop && styles.isTight)}
+        >
+          <div className={styles.station}>
+            <span>{status.train.origin.name}</span>
+            <span className={styles.time}>
+              ab {departureSchedule.planned}
+              {!departureSchedule.isOnTime && (
+                <span className={styles.delay}>
+                  &nbsp;<sup>+{departureSchedule.delayInMinutes}</sup>
+                </span>
+              )}
+            </span>
+          </div>
+        </div>
+
+        {showCurrentStop && currentStop && (
+          <div className={styles.upcoming}>
+            <div className={styles.station}>
+              <CurrentStop
+                destination={status.train.destination}
+                stops={stops}
+              />
+            </div>
+          </div>
+        )}
+
+        <div
+          className={clsx(
+            styles.destination,
+            !showCurrentStop && styles.withLine
+          )}
+        >
+          <div className={styles.station}>
+            <span>{status.train.destination.name}</span>
+            <span className={styles.time}>
+              an {arrivalSchedule.planned}
+              {!arrivalSchedule.isOnTime && (
+                <span className={styles.delay}>
+                  &nbsp;<sup>+{arrivalSchedule.delayInMinutes}</sup>
+                </span>
+              )}
+            </span>
+          </div>
+        </div>
+      </header>
+
+      <div className={styles.sheet}>
+        <ul className={styles.stats}>
+          <li>
+            <TbRoute size={20} />
+            <span>{Math.ceil(status.train.distance / 1000)} km</span>
+          </li>
+          <li>
+            <MdOutlineToken size={20} />
+            <span>{status.train.points} Punkte</span>
+          </li>
+          <li>
+            <MdCommit size={20} />
+            <span>{stops.length - 1} Stationen</span>
+          </li>
+        </ul>
       </div>
+    </main>
+  );
+};
 
-      <div className={styles.meta}>
-        <LineIndicator
-          className={styles.lineIndicator}
-          lineName={status.train.lineName}
-          product={status.train.category}
-          productName=""
-        />
+const CurrentStop = ({ destination, stops }: CurrentStopProps) => {
+  const [currentStop, setCurrentStop] = useState<Stop | undefined>(
+    findCurrentStop(stops, destination)
+  );
+  const arrival = useMemo(
+    () => new Date(currentStop?.arrivalReal!),
+    [currentStop?.arrivalReal]
+  );
+  const [remaining, setRemaining] = useState(getMinutesUntil(arrival));
 
-        <div>
-          <TbRoute size={18} />
-          <span>{Math.ceil(status.train.distance / 1000)} km</span>
-        </div>
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const minutes = getMinutesUntil(arrival);
+      setRemaining(minutes);
 
-        <div>
-          <MdOutlineToken size={18} />
-          <span>{status.train.points}</span>
-        </div>
-      </div>
+      if (minutes < 0) {
+        setCurrentStop(findCurrentStop(stops, destination));
+      }
+    }, 1000);
 
-      <div className={styles.destination}>
-        <div className={styles.station}>
-          <span>{status.train.destination.name}</span>
-          <span className={styles.time}>
-            an {arrivalSchedule.planned}
-            {!arrivalSchedule.isOnTime && (
-              <span className={styles.delay}>
-                &nbsp;<sup>+{arrivalSchedule.delayInMinutes}</sup>
-              </span>
-            )}
-          </span>
-        </div>
-      </div>
-    </section>
+    return () => clearInterval(interval);
+  }, [arrival, destination, stops]);
+
+  return (
+    <>
+      <span>{currentStop?.name}</span>
+      <span className={styles.time}>
+        {remaining === 0 ? 'jetzt' : `in ${Math.abs(remaining)} Min.`}
+      </span>
+    </>
   );
 };
 
--- src/components/StatusDetails/types.ts
+++ src/components/StatusDetails/types.ts
@@ -1,3 +1,10 @@
+import { Stop } from '@/traewelling-sdk/types';
+
+export type CurrentStopProps = {
+  destination: Stop;
+  stops: Stop[];
+};
+
 export type StatusDetailsProps = {
   id: string;
 };
