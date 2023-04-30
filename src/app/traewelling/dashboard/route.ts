54735cd8203b3f00264a188c0975aa2e49d48e26
nbank <bank@neoskop.de>
2023-04-30 17:47:48 +0200

    feat: use right dashboard endpoint

---
+++ src/app/traewelling/dashboard/route.ts
@@ -0,0 +1,25 @@
+import { authOptions } from '@/pages/api/auth/[...nextauth]';
+import { TraewellingSdk } from '@/traewelling-sdk';
+import createErrorResponse from '@/utils/api/createErrorResponse';
+import createResponse from '@/utils/api/createResponse';
+import { getServerSession } from 'next-auth';
+
+export async function GET(request: Request) {
+  try {
+    const session = await getServerSession(authOptions);
+
+    if (!session) {
+      return createErrorResponse({
+        error: 'No session',
+        statusCode: 401,
+      });
+    }
+
+    const data = await TraewellingSdk.dashboard.personal();
+    return createResponse({
+      body: data,
+    });
+  } catch (error) {
+    return createErrorResponse({ error });
+  }
+}
--- src/traewelling-sdk/index.ts
+++ src/traewelling-sdk/index.ts
@@ -1,6 +1,7 @@
 import * as auth from './functions/auth';
+import * as dashboard from './functions/dashboard';
 import * as status from './functions/status';
 import * as trains from './functions/trains';
 import * as user from './functions/user';
 
-export const TraewellingSdk = { auth, status, trains, user };
+export const TraewellingSdk = { auth, status, trains, user, dashboard };
