adb386820fc0afb8210e136acd2733b171b0b205
nbank <bank@neoskop.de>
2023-05-11 17:37:37 +0200

    fix: adjust file

--- src/app/_actions.ts
+++ src/app/_actions.ts
@@ -1,10 +1,10 @@
+'use server';
+
 import { TraewellingSdk } from '@/traewelling-sdk';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
 export const deleteStatus = async (id: string) => {
-  'use server';
-
   await TraewellingSdk.status.remove({ id });
 
   revalidatePath(`/status/${id}`);
