0a69f75b0e5dca10e28ca5ef6f275afc252dcbef
vainamov <fabian@marine.si>
2023-06-19 18:34:32 +0200

    wip: add basic status cards

--- src/components/Statuses/Statuses.tsx
+++ src/components/Statuses/Statuses.tsx
@@ -1,6 +1,5 @@
 import { useDashboard } from '@/hooks/useDashboard/useDashboard';
-import Image from 'next/image';
-import Link from 'next/link';
+import StatusCard from '../StatusCard/StatusCard';
 import styles from './Statuses.module.scss';
 
 const Statuses = () => {
@@ -14,43 +13,47 @@ const Statuses = () => {
   return (
     <div className={styles.base}>
       {statuses.map(
-        ({ id, username, profilePicture, body, likes, liked, type, train }) => {
-          return (
-            <div
-              key={id}
-              className={styles.status}
-              style={{
-                ['--current-status-color' as any]: `var(--color-${train.category})`,
-              }}
-            >
-              <div>
-                <Link href={`/u/${username}`}>{username}</Link>
-                <div>{train.lineName}</div>
+        // ({ id, username, profilePicture, body, likes, liked, type, train }) => {
+        //   return (
+        //     <div
+        //       key={id}
+        //       className={styles.status}
+        //       style={{
+        //         ['--current-status-color' as any]: `var(--color-${train.category})`,
+        //       }}
+        //     >
+        //       <div>
+        //         <Link href={`/u/${username}`}>{username}</Link>
+        //         <div>{train.lineName}</div>
 
-                <div>{body}</div>
-                <div>{likes}</div>
-                <div>{liked && '❤️'}</div>
-                <Link href={`/status/${id}`}>
-                  <div>{id}</div>
-                </Link>
-              </div>
-              <hr className={styles.hr} />
+        //         <div>{body}</div>
+        //         <div>{likes}</div>
+        //         <div>{liked && '❤️'}</div>
+        //         <Link href={`/status/${id}`}>
+        //           <div>{id}</div>
+        //         </Link>
+        //       </div>
+        //       <hr className={styles.hr} />
 
-              <div>
-                <div>
-                  <Image
-                    src={profilePicture}
-                    width={32}
-                    height={32}
-                    unoptimized
-                    className={styles.profilePicture}
-                    alt={`Profilbild von ${username}`}
-                  />
-                </div>
-              </div>
-            </div>
-          );
-        }
+        //       <div>
+        //         <div>
+        //           <Image
+        //             src={profilePicture}
+        //             width={32}
+        //             height={32}
+        //             unoptimized
+        //             className={styles.profilePicture}
+        //             alt={`Profilbild von ${username}`}
+        //           />
+        //         </div>
+        //       </div>
+        //     </div>
+        //   );
+        // }
+
+        (status) => (
+          <StatusCard key={status.id} status={status} />
+        )
       )}
     </div>
   );
