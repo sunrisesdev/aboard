96811124c1eac649844473354731b0f8c932ff1b
vainamov <fabian@marine.si>
2023-06-19 19:48:47 +0200

    feat: update status card

--- src/components/StatusCard/StatusCard.tsx
+++ src/components/StatusCard/StatusCard.tsx
@@ -1,7 +1,9 @@
 import { getWhiteLineTheme } from '@/helpers/getLineTheme/getLineTheme';
+import { parseSchedule } from '@/utils/parseSchedule';
 import Image from 'next/image';
+import Link from 'next/link';
+import { PRODUCT_ICONS } from '../CheckIn/consts';
 import LineIndicator from '../LineIndicator/LineIndicator';
-import Route from '../Route/Route';
 import ThemeProvider from '../ThemeProvider/ThemeProvider';
 import styles from './StatusCard.module.scss';
 import { StatusCardProps } from './types';
@@ -14,64 +16,67 @@ const StatusCard = ({ status }: StatusCardProps) => {
         ''
     ).getTime() <= Date.now();
 
+  const arrivalSchedule = parseSchedule({
+    actual:
+      status.train.destination.arrivalReal ??
+      status.train.destination.arrival ??
+      '',
+    planned: status.train.destination.arrivalPlanned ?? '',
+  });
+
+  const departureSchedule = parseSchedule({
+    actual:
+      status.train.origin.departureReal ?? status.train.origin.departure ?? '',
+    planned: status.train.origin.departurePlanned ?? '',
+  });
+
   return (
     <ThemeProvider
       theme={getWhiteLineTheme(status.train.number, status.train.category)}
     >
-      <article className={styles.base}>
-        <header className={styles.header}>
-          <Image
-            alt={`Profilbild von ${status.username}`}
-            className={styles.avatar}
-            height={32}
-            src={status.profilePicture}
-            style={{ flexShrink: 0 }}
-            unoptimized
-            width={32}
-          />
-
-          <div>
-            <div className={styles.username}>{status.username}</div>
-            <div style={{ fontSize: '0.875rem', marginTop: '-4px' }}>
-              {status.train.origin.name}
+      <Link className={styles.link} href={`/status/${status.id}`}>
+        <article className={styles.base}>
+          <header className={styles.header}>
+            <Image
+              alt={`Profilbild von ${status.username}`}
+              className={styles.avatar}
+              height={32}
+              src={status.profilePicture}
+              style={{ flexShrink: 0 }}
+              unoptimized
+              width={32}
+            />
+            <div>
+              <div className={styles.username}>{status.username}</div>
             </div>
-          </div>
-
-          {/* <div className={styles.time}>
-            {formatTime(new Date(status.createdAt))}
-          </div> */}
-        </header>
-
-        <section className={styles.route}>
-          <div style={{ marginTop: '-30px' }}>
-            <Route>
-              <Route.Entry
-                lineSlot={
-                  <Route.Line variant={hasArrived ? 'default' : 'hybrid'} />
-                }
-              >
-                <div style={{ height: '1.5rem' }} />
-              </Route.Entry>
-              <Route.Entry>{status.train.destination.name}</Route.Entry>
-            </Route>
-          </div>
-        </section>
-
-        <footer
-          style={{
-            alignItems: 'center',
-            display: 'flex',
-            padding: '0.5rem',
-          }}
-        >
-          <LineIndicator
-            lineId={status.train.number}
-            lineName={status.train.lineName}
-            product={status.train.category}
-            productName=""
-          />
-        </footer>
-      </article>
+            {PRODUCT_ICONS[status.train.category]({
+              className: styles.productIcon,
+            })}
+          </header>
+          <header className={styles.origin}>
+            <div className={styles.stop} />
+            <div>{status.train.origin.name}</div>
+            <div className={styles.time}>{departureSchedule.actual}</div>
+          </header>
+          <section className={styles.destination}>
+            <div className={styles.station}>
+              {status.train.destination.name}
+            </div>
+            <div className={styles.footer}>
+              <div className={styles.line} />
+              <LineIndicator
+                className={styles.lineIndicator}
+                lineId={status.train.number}
+                lineName={status.train.lineName}
+                product={status.train.category}
+                productName=""
+              />
+              <div className={styles.stop} />
+              <div className={styles.time}>{arrivalSchedule.actual}</div>
+            </div>
+          </section>
+        </article>
+      </Link>
     </ThemeProvider>
   );
 };
