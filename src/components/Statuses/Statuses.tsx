5873f3772c7f91357fbaad85917055209995f3da
LehDev <lars@lehdev.de>
2023-06-21 23:51:21 +0200

    feat: add dates to dashboard
    achieved by sorting statuses by dates and storing it in new array
    created new date class in Style-File

--- src/components/Statuses/Statuses.tsx
+++ src/components/Statuses/Statuses.tsx
@@ -1,6 +1,8 @@
 'use client';
 
 import { useDashboard } from '@/hooks/useDashboard/useDashboard';
+import { Status } from '@/traewelling-sdk/types';
+import { parseSchedule } from '@/utils/parseSchedule';
 import StatusCard from '../StatusCard/StatusCard';
 import styles from './Statuses.module.scss';
 
@@ -23,10 +25,45 @@ const Statuses = () => {
     return <div className={styles.base}>Keine Daten vorhanden</div>;
   }
 
+  //sort by date
+  const sortedStatuses: { dateKey: string; statuses: Status[] }[] = [];
+  statuses.forEach((status) => {
+    const departureSchedule = parseSchedule({
+      actual:
+        status.train.origin.departureReal ??
+        status.train.origin.departure ??
+        '',
+      planned: status.train.origin.departurePlanned ?? '',
+    });
+    const date = new Date(departureSchedule.actualValue);
+    const dateKey = `${date.getDate()}.${
+      date.getMonth() + 1
+    }.${date.getFullYear()}`;
+
+    const existingDate = sortedStatuses.find((s) => s.dateKey === dateKey);
+    if (existingDate) {
+      existingDate.statuses.push(status);
+    } else {
+      sortedStatuses.push({
+        dateKey,
+        statuses: [status],
+      });
+    }
+  });
+
   return (
-    <div className={styles.base}>
-      {statuses.map((status) => (
-        <StatusCard key={status.id} status={status} />
+    <div>
+      {sortedStatuses.map((s) => (
+        <div key={s.dateKey} className={styles.base}>
+          <div className={styles.date}>
+            <hr />
+            <span>{s.dateKey}</span>
+            <hr />
+          </div>
+          {s.statuses.map((status) => (
+            <StatusCard key={status.id} status={status} />
+          ))}
+        </div>
       ))}
     </div>
   );
