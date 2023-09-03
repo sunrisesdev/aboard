4b0958df5c3e694558b3b0314a0afcb7e1498e54
vainamov <fabian@marine.si>
2023-09-03 18:49:24 +0200

    feat: change check in options to native selects

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -2,4 +2,7 @@ import { Figtree, Inter, Source_Sans_3 } from 'next/font/google';
 
 export const figtree = Figtree({ subsets: ['latin'] });
 export const inter = Inter({ subsets: ['latin'] });
-export const sourceSans3 = Source_Sans_3({ subsets: ['latin'] });
+export const sourceSans3 = Source_Sans_3({
+  fallback: ['system-ui'],
+  subsets: ['latin'],
+});
